77110a1126e17d158a229717006256975921626f
Hans Kirchner <hans-kirchner@gmx.de>
2016-06-10 02:42:12 +0200

    Add router paths on server, fix facebook redirect

--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,23 @@
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('layout', { title: 'Hey', message: 'Hello there!'});
-    });
+    app.get('/', function(req, res) { res.render('layout'); });
+    app.get('/pilots', function(req, res) { res.render('layout'); });
+    app.get('/events', function(req, res) { res.render('layout'); });
+    app.get('/event/detail/:id', function(req, res) { res.render('layout'); });
+    app.get('/pilot/detail/:id', function(req, res) { res.render('layout'); });
+
 	app.get('/app.component.pug', function(req, res) {
-	res.render('app.component.pug', { title: 'Hey', message: 'Hello there!'});
+	res.render('app.component.pug');
 	});
 	app.get('/pilots.component.pug', function(req, res) {
-	res.render('pilots.component.pug', { title: 'Hey', message: 'Hello there!'});
+	res.render('pilots.component.pug');
 	});
 	app.get('/events.component.pug', function(req, res) {
-	res.render('events.component.pug', { title: 'Hey', message: 'Hello there!'});
+	res.render('events.component.pug');
 	});
 	app.get('/event-detail.component.pug', function(req, res) {
-	res.render('event-detail.component.pug', { title: 'Hey', message: 'Hello there!'});
+	res.render('event-detail.component.pug');
 	});
 	app.get('/pilot-detail.component.pug', function(req, res) {
-	res.render('pilot-detail.component.pug', { title: 'Hey', message: 'Hello there!'});
+	res.render('pilot-detail.component.pug');
 	});
 };
