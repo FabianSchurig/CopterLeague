b948543ef47173bc0a663af6ec2b251360ea0301
Bitfroest <fabian.schurig.94@gmail.com>
2016-06-16 04:00:24 +0200

    added put form for pilot-detail, minor fixes

--- app/pilot-detail.component.ts
+++ app/pilot-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RouteParams } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS }    from '@angular/http';
+import { NgForm }    from '@angular/common';
 
 import { Pilot } from './pilot';
 import { PilotService } from './pilot.service';
-import 'rxjs/Rx';
 
 import { Iso8601ToDatePipe } from './iso8601.pipe';
+import { isLoggedin, pilotId } from './is-loggedin';
+import 'rxjs/Rx';
+import * as moment from 'moment';
+
 
 @Component({
 	selector: 'my-pilot-detail',
@@ -15,18 +19,60 @@ import { Iso8601ToDatePipe } from './iso8601.pipe';
 })
 export class PilotDetailComponent implements OnInit {
 	pilot: Pilot;
+	error = false;
+	submitted = false;
+	@Input() loggedIn;
+	id: number = pilotId();
+	isEditable= false;
+	active = false;
+	errorMessage = '';
 
-	constructor(private pilotService: PilotService,
-		private routeParams: RouteParams) {
+	constructor(
+		private pilotService: PilotService,
+		private routeParams: RouteParams
+	) {
+		this.getPilot();
 	}
 	
 	getPilot() {
 		let id = +this.routeParams.get('id');
-		this.pilotService.getPilot(id).subscribe(pilot => this.pilot = pilot, error =>  this.errorMessage = <any>error);
+		this.pilotService.getPilot(id).subscribe(pilot => {
+			this.pilot = pilot;
+			if(this.pilot.id == this.id){
+				this.active = true;
+			}
+		}, error =>  this.errorMessage = <any>error);
+	}
+	
+	edit(){
+		this.isEditable = true;
+		console.log('edit');
 	}
 
-	ngOnInit() {
-		this.getPilot();
+	ngOnInit() {	
+		this.id = pilotId();	
+	}
+	
+	onSubmit() {
+		var uPilot = new Pilot();
+		uPilot.alias = this.pilot.alias;
+		uPilot.firstName = this.pilot.firstName;
+		uPilot.familyName = this.pilot.familyName;
+		uPilot.email = this.pilot.email;
+		uPilot.telephone = this.pilot.telephone;
+		uPilot.notes = this.pilot.notes;
+		uPilot.id = this.pilot.id;
+		uPilot.street = this.pilot.street;
+		uPilot.plz = this.pilot.plz;
+		uPilot.city = this.pilot.city;
+		
+		this.pilotService
+			.updatePilot(uPilot)
+			.subscribe(pilot => {
+				this.pilot = pilot;
+				this.isEditable = false;
+				}
+				, error => this.errorMessage = <any>error);
 	}
 
 	goBack() {
