cb6204b636c6b16f03cf1615d6ecc346d2ca6fe5
Bitfroest <fabian.schurig.94@gmail.com>
2016-06-21 18:04:33 +0200

    added map service for new map api

--- app/map.component.ts
+++ app/map.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouteParams, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { HTTP_PROVIDERS }		from '@angular/http';
 
-import { EventService } from './event.service';
-import { Event } from './event';
+import { MapService } from './map.service';
 import 'rxjs/Rx';
 
 import { Iso8601ToDatePipe } from './iso8601.pipe';
@@ -12,57 +11,72 @@ import { Iso8601ToDatePipe } from './iso8601.pipe';
 	selector: 'map',
 	templateUrl: 'map.component.html',
 	directives: [ROUTER_DIRECTIVES],
-	providers: [EventService, Iso8601ToDatePipe]
+	providers: [MapService, Iso8601ToDatePipe]
 })
 export class MapComponent implements OnInit {
-	events: Event[];
+	marker: Marker[];
 	myLatlng = {lat: 51.163 , lng: 10.447};
 	map: string = "";
 	locations:string[][];
 	i: number = 0;
 
 	
-	constructor(private eventService: EventService, private router: Router) { 
+	constructor(private mapService: MapService, private router: Router) { 
 		this.locations = [];
+		this.marker = [];
 		this.map = "";
 	}
 
-	getEvents() {
+	getMarkers() {
 		this.i = 0;
-		this.eventService.getEvents().subscribe(events => {
-			this.events = events;	
+		this.mapService.getMarker().subscribe(marker => {
+			this.marker = marker.data;	
 		
-			for(var item of this.events.data){
+			for(var item of this.marker){
 				if(item.location != null) {
-					var string = item.location.split(",",2);
 					console.log(item.location);
-					console.log(string);
 					this.locations[this.i] = [];
-					this.locations[this.i][0] = new google.maps.LatLng(parseFloat(string[0]),parseFloat(string[1]));
+					this.locations[this.i][0] = new google.maps.LatLng(parseFloat(item.lat),parseFloat(item.lng));
 					console.log(this.myLatLng);
-					let date = new Iso8601ToDatePipe().transform(item.date);
-					let deadline = new Iso8601ToDatePipe().transform(item.deadline);
+					
+					var string = '';
+					if(item.type == 'pilot'){
+						string = '<i class="fa fa-user" aria-hidden="true"></i>';
+					}else if(item.type == 'event'){
+						string = '<i class="fa fa-trophy" aria-hidden="true"></i>';
+					}
 					
 					this.locations[this.i][1] = '<div id="content">'+
 						'<div id="siteNotice">'+
 						'</div>'+
-						'<h1 id="firstHeading" class="firstHeading">'+item.title+'</h1>'+
+						'<h2 id="firstHeading" class="firstHeading">'+string+' '+item.name+'</h1>'+
 						'<div id="bodyContent">'+
-						'<table style="width: 300px"><tr><td><p><b>Datum: </b></p></td><td><p class="pull-right">'+ date + '</p></td></tr>' +
-						'<tr><td><p><b>Anmeldefrist: </b></p></td><td><p class="pull-right">'+ deadline + '</p></td></tr></table>' +
-						'<a class="pull-right" href="/event/detail/'+item.id+'">'+
+						'<a class="pull-right" href="/'+item.type+'/detail/'+item.id+'">'+
 						'mehr Details</a> '+
 						'</div>'+
 						'</div>';
 						
-					this.locations[this.i][2] = item.title;
+					this.locations[this.i][2] = item.name;
 					this.locations[this.i][3] = item.id;
+					this.locations[this.i][4] = item.type;
 					this.i++;
 				}
 			}
 			
-			var pinColor = "0066FF";
-			var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+			var pinColorPilot = "0066FF";
+			var pinImagePilot = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColorPilot,
+				new google.maps.Size(21, 34),
+				new google.maps.Point(0,0),
+				new google.maps.Point(10, 34));
+			
+			var pinColorEvent = "66ffff";
+			var pinImageEvent = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColorEvent,
+				new google.maps.Size(21, 34),
+				new google.maps.Point(0,0),
+				new google.maps.Point(10, 34));
+				
+			var pinColorCustom = "00ff00";
+			var pinImageCustom = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColorCustom,
 				new google.maps.Size(21, 34),
 				new google.maps.Point(0,0),
 				new google.maps.Point(10, 34));
@@ -72,6 +86,14 @@ export class MapComponent implements OnInit {
 			var marker, i, content;
 
 			for (i in this.locations) {	
+				var pinImage;
+				if(this.locations[i][4] == 'pilot'){
+					pinImage = pinImagePilot;
+				}else if(this.locations[i][4] == 'event'){
+					pinImage = pinImageEvent;
+				}else{
+					pinImage = pinImageCustom;
+				}
 				marker = new google.maps.Marker({
 					position: this.locations[i][0],
 					map: this.map
@@ -94,7 +116,7 @@ export class MapComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.getEvents();
+		this.getMarkers();
 		var myOptions = {
 				zoom: 7,
 				center: {lat: 51.163 , lng: 10.447},
@@ -102,9 +124,5 @@ export class MapComponent implements OnInit {
 			};
 			
 		this.map = new google.maps.Map(document.getElementById('map'), myOptions);
-	}
-
-	onSelect(event: Event) { 
-		this.selectedEvent = event;
 	}
 }
---
+++ app/map.service.ts
@@ -0,0 +1,37 @@
+import { Injectable }     from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class MapService {
+	token: String;
+	id: number;
+	
+	constructor (private http: Http) {
+	}
+
+	private mapUrl = 'api/map';  // URL to web API
+
+	getMarker () {
+		console.log('getMap');
+		return this.http.get(this.mapUrl)
+					.map(this.extractData)
+					.catch(this.handleError);
+	}
+	private extractData(res: Response) {
+		if (res.status < 200 || res.status >= 300) {
+			throw new Error('Response status: ' + res.status);
+		}
+		let body = res.json();
+		console.log(body);
+		return body || [];
+	}
+	private handleError (error: any) {
+		// In a real world app, we might use a remote logging infrastructure
+		let errMsg = error.message || 'Server error';
+		console.error(errMsg); // log to console instead
+		return Observable.throw(errMsg);
+	}
+}
